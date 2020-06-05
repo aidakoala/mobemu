37c671f8ada7acc7efdf83c3c8b320eeecb58d60
aidastefaniamanole <aida.manole@stud.acs.upb.ro>
2020-06-05 15:43:16 +0300

    gcloud 1h trace Epidemic

--- src/mobemu/MobEmu.java
+++ src/mobemu/MobEmu.java
@@ -125,19 +125,19 @@ public class MobEmu {
 
 		Node[] nodes = new Node[parser.getNodesNumber()];
 		for (int i = 0; i < nodes.length; i++) {
-//			nodes[i] = new Epidemic(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], dataMemory,
-//					100, seed, parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), dissemination,
-//					altruism);
+			nodes[i] = new Epidemic(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], dataMemory,
+					100, seed, parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), dissemination,
+					altruism);
 //			nodes[i] = new SprayAndWait(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], 5000,
 //					100, seed,parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), dissemination, altruism,
 //					SprayAndWait.Type.BINARY);
 //			nodes[i] = new SprayAndFocus(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], 5000,
 //					100, seed,parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), altruism,
 //					Node.MILLIS_IN_10MIN);
-			nodes[i] = new MySprayAndWait(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], 5000,
-					100, seed,parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), dissemination, altruism);
-			nodes[i].setCommonFriends(parser.getCommonFriends(i));
-			System.out.println(parser.getCommonFriends(i)[0]);
+//			nodes[i] = new MySprayAndWait(i, nodes.length, parser.getContextData().get(i), parser.getSocialNetwork()[i], 5000,
+//					100, seed,parser.getTraceData().getStartTime(), parser.getTraceData().getEndTime(), dissemination, altruism);
+//			nodes[i].setCommonFriends(parser.getCommonFriends(i));
+//			System.out.println(parser.getCommonFriends(i)[0]);
 		}
 
 		System.out.println("Generated nodes");
